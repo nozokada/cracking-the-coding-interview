5f013e6a345271aa9892558e0a96fdb001401171
Nozomi Okada <nozokada@gmail.com>
2020-07-28 22:48:15 -0600

    Changing method name

--- 1_1.py
+++ 1_1.py
@@ -1,4 +1,4 @@
-def all_chars_unique(string: str):
+def are_all_chars_unique(string: str):
     str_list = list(string)
     str_list.sort()
     for index in range(len(str_list) - 1):
@@ -9,4 +9,4 @@ def all_chars_unique(string: str):
 
 test_string = '1234567890'
 print(list(range(len(test_string))))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(all_chars_unique(test_string))
+print(are_all_chars_unique(test_string))
