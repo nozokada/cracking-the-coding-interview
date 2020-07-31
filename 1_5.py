91e65857593fa8408922e35278bb74dcd343128c
Nozomi Okada <nozokada@gmail.com>
2020-07-31 00:00:06 -0600

    Adding 1.5

---
+++ 1_5.py
@@ -0,0 +1,18 @@
+def compress(string: str):
+    result_str_list = []
+    prev_char = string[0]
+    count = 1
+    for index in range(1, len(string)):
+        if string[index] == prev_char:
+            count += 1
+        else:
+            result_str_list.append(f'{prev_char}{count}')
+            prev_char = string[index]
+            count = 1
+    result_str_list.append(f'{prev_char}{count}')
+    result_string = ''.join(result_str_list)
+    return result_string if len(result_string) < len(string) else string
+
+
+test_string = 'aabcccccaaa'
+print(compress(test_string))
