18a3e38fe4788fb005cc03c27a7798f0e57176bb
Nozomi Okada <nozokada@gmail.com>
2020-08-05 00:24:49 -0600

    Adding 2.1

---
+++ 2.1.py
@@ -0,0 +1,31 @@
+from linked_list import Node
+
+
+def delete_duplicates(node: Node):
+    n = node
+    duplicates_set = {node.data}
+    while n.tail is not None:
+        if n.tail.data in duplicates_set:
+            n.tail = n.tail.tail
+            continue
+        n = n.tail
+        duplicates_set.add(n.data)
+    return node
+
+
+def delete_duplicates_in_place(node: Node):
+    i = node
+    while i.tail is not None:
+        j = i
+        while j.tail is not None:
+            if i.data == j.tail.data:
+                j.tail = j.tail.tail
+                continue
+            j = j.tail
+        i = i.tail
+    return node
+
+
+linked_list = Node(1, Node(2, Node(3, Node(1))))
+unique_linked_list = delete_duplicates(linked_list)
+print(unique_linked_list.get_data())
