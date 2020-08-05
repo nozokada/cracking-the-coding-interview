76dc7aad62aa25074dbc0f51e0728306be159081
Nozomi Okada <nozokada@gmail.com>
2020-08-04 23:12:58 -0600

    Implementing delete method

--- linked_list.py
+++ linked_list.py
@@ -10,6 +10,18 @@ class Node:
             node = node.tail
         node.tail = end
 
+    def delete(self, data: int):
+        head = self
+        node = head
+        if node.data == data:
+            return node.tail
+        while node.tail is not None:
+            if node.tail.data == data:
+                node.tail = node.tail.tail
+                return head
+            node = node.tail
+        return head
+
     def get_data(self):
         if self.tail is None:
             return f'{self.data}'
@@ -17,5 +29,9 @@ class Node:
 
 
 linked_list = Node(1, Node(2, Node(3, Node(4))))
+
 linked_list.append(5)
 print(linked_list.get_data())
+
+another_linked_list = linked_list.delete(1)
+print(another_linked_list.get_data())
