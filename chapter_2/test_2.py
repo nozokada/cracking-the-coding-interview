38fbf45c2e0232ca1857caa87073231777eb569c
Nozomi Okada <nozokada@gmail.com>
2020-08-26 22:44:50 -0600

    Renaming tests

--- chapter_2/test_2.py
+++ chapter_2/test_2.py
@@ -25,14 +25,14 @@ class TestLinkedList(unittest.TestCase):
 
 
 class Test_2_1(unittest.TestCase):
-    def test_delete_duplicates_in_linked_list(self):
+    def test_delete_duplicates(self):
         linked_list = Node(1, Node(2, Node(3, Node(1))))
         self.assertEqual(Node(1, Node(2, Node(3))), delete_duplicates(linked_list))
         self.assertEqual(Node(1, Node(2, Node(3))), delete_duplicates_in_place(linked_list))
 
 
 class Test_2_2(unittest.TestCase):
-    def test_get_last_from(self):
+    def test_find_last_element_from(self):
         linked_list = Node(1, Node(2, Node(3, Node(4))))
         self.assertEqual(4, get_size(linked_list))
         self.assertEqual(4, find_last_element_from(linked_list, 0))
