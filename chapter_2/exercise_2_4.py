34a023fb4aee67a6ef0644c2f601e4c88a8b11a9
Nozomi Okada <nozokada@gmail.com>
2020-09-28 23:37:30 -0600

    Changed variable names

--- chapter_2/exercise_2_4.py
+++ chapter_2/exercise_2_4.py
@@ -2,27 +2,27 @@ from linked_list import Node
 
 
 def partition(value: int, node: Node):
-    smaller_nodes = None
-    bigger_nodes = None
-    partition_node = None
+    n_smaller = None
+    n_bigger = None
+    n_partition = None
 
     n = node
     while n:
         if n.data >= value:
-            if bigger_nodes:
-                bigger_nodes.append(n.data)
+            if n_bigger:
+                n_bigger.append(n.data)
             else:
-                bigger_nodes = Node(n.data)
+                n_bigger = Node(n.data)
         else:
-            if smaller_nodes:
-                partition_node = smaller_nodes.append(n.data)
+            if n_smaller:
+                n_partition = n_smaller.append(n.data)
             else:
-                smaller_nodes = Node(n.data)
-                partition_node = smaller_nodes
+                n_smaller = Node(n.data)
+                n_partition = n_smaller
         n = n.tail
 
-    if not smaller_nodes:
-        return bigger_nodes
+    if not n_smaller:
+        return n_bigger
 
-    partition_node.tail = bigger_nodes
-    return smaller_nodes
+    n_partition.tail = n_bigger
+    return n_smaller
