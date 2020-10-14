dfcbf14afa56e32a9e4668deac0ddc9083556d41
Nozomi Okada <nozokada@gmail.com>
2020-10-13 22:57:51 -0600

    Cleaned up a bit

--- generator/generator.py
+++ generator/generator.py
@@ -1,15 +1,31 @@
-def fibonacci():
-    a, b = 0, 1
-    yield a
-    yield b
-    while True:
-        a, b = b, a + b
+def fibonacci_gen(n):
+
+    def fibonacci():
+        a, b = 0, 1
+        yield a
         yield b
+        while True:
+            a, b = b, a + b
+            yield b
+
+    gen = fibonacci()
+    i = 0
+    for f in gen:
+        if i == n:
+            return f
+        i += 1
 
 
-n = 0
-for f in fibonacci():
-    print(f)
-    if n == 10:
-        break
-    n += 1
+def fibonacci_r(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fibonacci_r(n-1) + fibonacci_r(n-2)
+
+
+f_gen = fibonacci_gen(10)
+print(f_gen)
+f_r = fibonacci_r(10)
+print(f_r)
