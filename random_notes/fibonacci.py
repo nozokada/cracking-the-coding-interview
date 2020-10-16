4e56b3add87fa62a1a472875f12ebff6b9865b13
Nozomi Okada <nozokada@gmail.com>
2020-10-16 09:46:16 -0600

    Added another function fibonacci check

--- random_notes/fibonacci.py
+++ random_notes/fibonacci.py
@@ -1,21 +1,21 @@
-def fibonacci_gen(n):
-
-    def fibonacci():
-        a, b = 0, 1
-        yield a
-        yield b
-        while True:
-            a, b = b, a + b
-            yield b
-
+def fibonacci_g(n: int):
     i = 0
-    for f in fibonacci():
+    for f in fibonacci_gen():
         if i == n:
             return f
         i += 1
 
 
-def fibonacci_r(n):
+def fibonacci_gen():
+    a, b = 0, 1
+    yield a
+    yield b
+    while True:
+        a, b = b, a + b
+        yield b
+
+
+def fibonacci_r(n: int):
     if n == 0:
         return 0
     if n == 1:
@@ -24,7 +24,28 @@ def fibonacci_r(n):
     return fibonacci_r(n-1) + fibonacci_r(n-2)
 
 
-f_gen = fibonacci_gen(10)
-print(f_gen)
-f_r = fibonacci_r(10)
-print(f_r)
+# Return a list of specified number of Fibonacci numbers
+# which come after a certain Fibonacci number
+def fibonacci_check(number: int, count: int) -> list:
+    fibonacci_generator = fibonacci_gen()
+    fibonacci_list = []
+    for n in fibonacci_generator:
+        if n > number:
+            print(f'{number} is not a fibonacci number')
+            return []
+        if n == number:
+            fibonacci_list.append(n)
+            break
+
+    i = 1
+    for n in fibonacci_generator:
+        fibonacci_list.append(n)
+        i += 1
+        if i == count:
+            break
+    return fibonacci_list
+
+
+print(fibonacci_g(10))
+print(fibonacci_r(10))
+print(fibonacci_check(55, 10))
