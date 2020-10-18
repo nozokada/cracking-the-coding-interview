3cd673cf1d711252e16632f1837e821d2c77c196
Nozomi Okada <nozokada@gmail.com>
2020-10-17 22:40:25 -0600

    Fixed search methods for binary trees

--- chapter_4/search.py
+++ chapter_4/search.py
@@ -12,22 +12,28 @@ def search_dft_r(root: Node):
 
 
 def search_dft_stack(root: Node):
+    root.visited = True
     stack = [root]
     while stack:
         node = stack.pop()
-        node.visited = True
         print(node.data)
         for n in node.adjacent:
             if not n.visited:
+                node.visited = True
                 stack.append(n)
 
 
 def search_bft_queue(root: Node):
+    root.visited = True
     queue = [root]
     while queue:
         node = queue.pop(0)
-        node.visited = True
         print(node.data)
         for n in node.adjacent:
             if not n.visited:
+                node.visited = True
                 queue.append(n)
+
+
+tree = Node(1, Node(2, Node(3), Node(4)), Node(5, Node(6), Node(7)))
+search_bft_queue(tree)
