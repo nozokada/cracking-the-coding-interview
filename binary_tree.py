10476a74b2735189f5924de86d2cfb993ea72b1a
Nozomi Okada <nozokada@gmail.com>
2020-10-20 00:13:54 -0600

    Added print method and test for binary trees

--- binary_tree.py
+++ binary_tree.py
@@ -8,3 +8,35 @@ class Node:
     @property
     def adjacent(self):
         return [n for n in (self.left, self.right) if n]
+
+    def print(self):
+        print_binary_tree(self)
+
+
+def print_binary_tree(root: Node):
+    current_level = [root]
+    height = get_height(root)
+    padding = 2 ** (height - 1) - 1
+    while True:
+        if height == 0:
+            break
+        next_level = []
+        for n in current_level:
+            print(" " * padding, end='')
+            if not n:
+                print(" ", end='')
+            else:
+                print(n.data, end='')
+                next_level.append(n.left)
+                next_level.append(n.right)
+            print(" " * (padding + 1), end='')
+        print()
+        current_level = next_level
+        height -= 1
+        padding = 2 ** (height - 1) - 1
+
+
+def get_height(root: Node):
+    if not root:
+        return 0
+    return max(get_height(root.left), get_height(root.right)) + 1
--- chapter_4/test_4.py
+++ chapter_4/test_4.py
@@ -5,6 +5,22 @@ from chapter_4.exercise_4_1 import is_balanced
 from chapter_4.exercise_4_2 import is_route
 
 
+class TestBinaryTree(unittest.TestCase):
+    def test_print(self):
+        tree = Node(1,
+                    Node(2,
+                         Node(3,
+                              Node(4), Node(5)),
+                         Node(6,
+                              Node(7), Node(8))),
+                    Node(9,
+                         Node(10,
+                              Node(11), Node(12)),
+                         Node(13,
+                              Node(14), Node(15))))
+        tree.print()
+
+
 class Test_4_1(unittest.TestCase):
     def test_is_balanced(self):
         tree = Node(1, Node(2, Node(3), Node(4)), Node(5, Node(6), Node(7)))
