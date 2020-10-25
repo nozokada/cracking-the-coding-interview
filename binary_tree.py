371c311bcaf1dc7eb97b75920c7e457ffd1bd835
Nozomi Okada <nozokada@gmail.com>
2020-10-25 00:31:55 -0600

    Added parent property to BSTNode

--- binary_tree.py
+++ binary_tree.py
@@ -1,8 +1,9 @@
 class BSTNode:
-    def __init__(self, data, left=None, right=None):
+    def __init__(self, data, left=None, right=None, parent=None):
         self.data = data
         self.left = left
         self.right = right
+        self.parent = parent
         self.visited = False
 
     @property
--- chapter_4/exercise_4_3.py
+++ chapter_4/exercise_4_3.py
@@ -7,7 +7,7 @@ def build_bst_from_list(ascending_list: list, root: BSTNode = None):
 
     middle_index = len(ascending_list) // 2
     middle = ascending_list.pop(middle_index)
-    root = insert_node(root, middle)
+    root = insert_node(root, None, middle)
 
     build_bst_from_list(ascending_list[:middle_index], root)
     build_bst_from_list(ascending_list[middle_index:], root)
@@ -15,13 +15,13 @@ def build_bst_from_list(ascending_list: list, root: BSTNode = None):
     return root
 
 
-def insert_node(root: BSTNode, data: int):
+def insert_node(root: BSTNode, parent: BSTNode, data: int):
     if not root:
-        return BSTNode(data)
+        return BSTNode(data, parent=parent)
 
     if data < root.data:
-        root.left = insert_node(root.left, data)
+        root.left = insert_node(root.left, root, data)
     else:
-        root.right = insert_node(root.right, data)
+        root.right = insert_node(root.right, root, data)
 
     return root
