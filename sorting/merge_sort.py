0d44d04066cb0bd8636ecae58d65d1cd788f3c6e
Nozomi Okada <nozokada@gmail.com>
2020-11-07 21:11:18 -0700

    Added merge_sort.py

---
+++ sorting/merge_sort.py
@@ -0,0 +1,36 @@
+def sort(data: list):
+    size = len(data)
+    if size < 2:
+        return data
+
+    left = sort(data[:size // 2])
+    right = sort(data[size // 2:])
+
+    i = 0
+    j = 0
+    k = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            data[k] = left[i]
+            i += 1
+        else:
+            data[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        data[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        data[k] = right[j]
+        j += 1
+        k += 1
+
+    return data
+
+
+list_ = [5, 5, 3, 5, 9, 1, 4, 5]
+print(list_)
+sort(list_)
+print(list_)
