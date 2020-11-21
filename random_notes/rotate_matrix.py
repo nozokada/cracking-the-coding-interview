1066e1e7cedc1d397c5295fc7f8613d19af86923
Nozomi Okada <nozokada@gmail.com>
2020-11-21 14:03:41 -0700

    Added rotate_matrix_counterclockwise

--- random_notes/rotate_matrix.py
+++ random_notes/rotate_matrix.py
@@ -1,4 +1,4 @@
-def rotate_matrix(matrix: list):
+def rotate_matrix_clockwise(matrix: list):
     matrix.reverse()
     for i in range(len(matrix)):
         for j in range(i):
@@ -6,9 +6,29 @@ def rotate_matrix(matrix: list):
     return matrix
 
 
-m = [[1, 2, 3],
-     [4, 5, 6],
-     [7, 8, 9]]
+def rotate_matrix_counterclockwise(matrix: list):
+    matrix.reverse()
+    for i in range(len(matrix)):
+        for j in range(len(matrix) - i):
+            i_ = len(matrix) - 1 - i
+            j_ = len(matrix) - 1 - j
+            matrix[i][j], matrix[j_][i_] = matrix[j_][i_], matrix[i][j]
+    return matrix
 
-print(m)
-print(rotate_matrix(m))
+
+def print_matrix(matrix: list):
+    for row in matrix:
+        for val in row:
+            print(val, end='\t')
+        print()
+    print()
+
+
+m = [[1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 0, 1, 2],
+     [3, 4, 5, 6]]
+
+print_matrix(m)
+print_matrix(rotate_matrix_clockwise(m))
+print_matrix(rotate_matrix_counterclockwise(m))
